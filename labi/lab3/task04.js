14854091b1c2979e74be97ffe10aa0f8f2ff7751
Maxim <68068289+BarbarisEmpire@users.noreply.github.com>
2021-07-05 00:22:45 +0500

    Add files via upload

---
+++ labi/lab3/task04.js
@@ -0,0 +1,11 @@
+﻿const ut = require('./ut00');
+
+const groupName = 'ПИб-1';
+
+const groupId = ut.getGroupId(groupName);
+
+console.log(
+    ut.getAdultStudents()
+        .filter(g => g.idGr === groupId)
+)
+
